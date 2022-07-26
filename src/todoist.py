b038c1a7ee2cb9d045d6a732a181e7edf8dbff82
Karlo Milicic <kmilicic@student.manchester.ac.uk>
2022-07-26 16:48:33 +0100

    enter label

---
+++ src/todoist.py
@@ -0,0 +1,59 @@
+from todoist_api_python.api import TodoistAPI
+from todoist_api_python.models import Project
+from typing import List
+
+from src.imdb import Movie
+from src.parse_input import parse_bool
+
+
+def is_valid_api_key(TODOIST_API_KEY: str) -> bool:
+    "Checks if the given API KEY is valid."
+    todoist = TodoistAPI(TODOIST_API_KEY)
+    try:
+        todoist.get_projects()
+        return True
+    except:
+        return False
+
+
+class TodoistPort():
+    def __init__(self, TODOIST_API_KEY: str, default_project_id: str) -> None:
+        self.todoist = TodoistAPI(TODOIST_API_KEY)
+        self.default_project_id = default_project_id
+
+    def get_projects(self) -> Project:
+        projects = self.todoist.get_projects()
+
+        print("Pick the default project where to place all tasks.")
+        for i, project in enumerate(projects):
+            print(f"{i + 1}. {project.name}")
+        project_index = int(input("Select the project by typing leading number: ")) - 1
+        return projects[project_index]
+
+    def get_label_id(self, label_name: str) -> str:
+        "TODO: Use some kind of cache"
+        labels = self.todoist.get_labels()
+        for label in labels:
+            if label.name == label_name:
+                return label.id
+        print(f"Couldn't find label with name: {label_name}.")
+        ans = parse_bool("Would you like to create it?", default_value=False)
+        if not ans:
+            print("Exiting script.")
+            quit()
+        label = self.todoist.add_label(label_name)
+        return label.id
+
+    def make_task(self, movie: Movie, labels: List[str]) -> None:
+        content = f"[{movie.title}]({movie.link})"
+        description = movie.plot
+        if labels:
+            label_ids = [self.get_label_id(label) for label in labels]
+            self.todoist.add_task(content=content,
+                                  description=description,
+                                  project_id=self.default_project_id,
+                                  label_ids=label_ids)
+        else:
+            self.todoist.add_task(content=content,
+                                  description=description,
+                                  project_id=self.default_project_id)
