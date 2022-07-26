3d3915fa7dd8dd3f5d5fd941c2302309a867c0f4
Karlo Milicic <kmilicic@student.manchester.ac.uk>
2022-07-26 19:35:02 +0100

    updated exception to support optional message

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,7 +1,15 @@
+from typing import Optional
+
+
 class MissingAttributeException(AttributeError):
-    def __init__(self, missing_attribute: str, *args: object) -> None:
+    def __init__(self, missing_attribute: str, message: Optional[str],
+                 *args: object) -> None:
         super().__init__(*args)
+        self.message = message
         self.missing_value = missing_attribute
 
     def __str__(self) -> str:
-        return f"Missing required attribute: {self.missing_value}"
+        message = f"Missing required attribute: {self.missing_value}"
+        if self.message:
+            message += f"\n{self.message}"
+        return message
